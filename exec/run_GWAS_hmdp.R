10f5b52067777d1519d2f2f54c589f5a1d9262b3
Gautam Sabnis <sabnig@sumner-bld03.sumner2.jax.org>
2024-11-26 15:19:45 -0500

    Edited the run_GWAS_hmdp.R to load snps_inbred_bxd.csv.gz file.

--- exec/run_GWAS_hmdp.R
+++ exec/run_GWAS_hmdp.R
@@ -189,7 +189,7 @@ complete.geno <- NULL
 
 #for (f in args$genotypes){
 #f <- "/projects/kumar-lab/sabnig/GWAS2/example/snps_bxd_inbred.csv"
-f <- system.file("extdata", "snps_hmdp2.csv.gz", package = "mousegwas")
+f <- system.file("extdata", "snps_inbred_bxd.csv.gz", package = "mousegwas")
 geno <- fread(f)
 geno[, c("major", "minor") := tstrsplit(observed, "/", fixed=TRUE, keep=1:2)]
 geno <- geno[rs!="",]
